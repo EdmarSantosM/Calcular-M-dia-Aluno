5a0ca47e8cc08bb170ebb4a6a2fe161310cee4cf
EdmarSantosM <santosem4@hotmail.com>
2023-05-02 23:06:32 -0300

    Update Program.cs
    Entrada do  nome  utilizando  o método ReadLine

--- CalcularMedia/Program.cs
+++ CalcularMedia/Program.cs
@@ -5,6 +5,11 @@ class Program
 {
     static void Main(string[] args)
     {
+        System.Console.WriteLine();
+        Console.Write("\nDigite seu nome: ");
+        string nome = Console.ReadLine();
+        System.Console.WriteLine("\n------------------------------------------------------------------");
+
         
     }
 }
