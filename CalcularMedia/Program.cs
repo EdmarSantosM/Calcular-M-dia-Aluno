59d840270fe63282a743ad0665d7fcc8a9df5d5b
EdmarSantosM <santosem4@hotmail.com>
2023-05-02 23:12:20 -0300

    Update Program.cs
    Recebendo as notas informadas pelo usuário.
    
    Utilizado float.Parse para  conversão do valor  recebido como string

--- CalcularMedia/Program.cs
+++ CalcularMedia/Program.cs
@@ -10,6 +10,18 @@ class Program
         string nome = Console.ReadLine();
         System.Console.WriteLine("\n------------------------------------------------------------------");
 
+        Console.Write("\nDigite a primeira nota:");
+        float nota1 =(float.Parse(Console.ReadLine()));
+
+        Console.Write("\nDigite a segunda  nota:");
+        float nota2 = (float.Parse(Console.ReadLine()));
+
+        Console.Write("\nDigite a terceira nota:");
+        float nota3 =(float.Parse(Console.ReadLine()));
+
+        Console.Write("\nDigite a quarta  nota:");
+        float nota4 = (float.Parse(Console.ReadLine()));
+
         
     }
 }
